78262798e381d3755dfeac4594c1e76c6083ce6c
naturalnet <52394214+naturalnet@users.noreply.github.com>
2025-08-28 23:22:32 +0200

    Update safety.js

--- packages/qa/safety.js
+++ packages/qa/safety.js
@@ -1,1 +1,5 @@
-
+export function validateSafety(normalized) {
+const errs = [];
+if (!normalized?.legal_entities?.length) errs.push('legal_entities missing');
+return { ok: errs.length === 0, errs };
+}
