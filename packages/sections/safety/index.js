50048b2eb6d1018056d3e5ed937ee0d97edfc608
naturalnet <52394214+naturalnet@users.noreply.github.com>
2025-08-28 23:52:49 +0200

    Update index.js

--- packages/sections/safety/index.js
+++ packages/sections/safety/index.js
@@ -1,13 +1,13 @@
+// packages/sections/safety/index.js
 import { extractAdmiralsSafety } from '../../brokers/admirals/safety.js';
 
-
-export async function extract({ brokerSlug, ctx }) {
-switch (brokerSlug) {
-case 'admirals':
-case 'admiralmarkets':
-case 'admiral':
-return await extractAdmiralsSafety();
-default:
-return null; // other brokers will be added next
-}
+export async function extract({ brokerSlug }) {
+  switch ((brokerSlug || '').toLowerCase()) {
+    case 'admirals':
+    case 'admiralmarkets':
+    case 'admiral':
+      return await extractAdmiralsSafety();
+    default:
+      return null;
+  }
 }
